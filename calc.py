dee3d79880f7af3006519f2ccda4a071f2d941ef
anyango55 <43138367+anyango55@users.noreply.github.com>
2019-04-15 17:26:44 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,33 @@
+def add(a,b):
+	answer = a+b
+	return answer
+
+	def subtract(a,b):
+	answer = a-b
+	return answer
+
+	def multiply(a,b):
+	answer = a*b
+	return answer
+
+	def divide(a/b):
+	answer = a/b
+	return answer
+
+	def module(a,b):
+	answer = a%b
+	return answer
+
+	def exponent(a,b):
+	answer = a**b
+	return answer
+
+	def percentage(a,b):
+	answer = a%b
+	return answer
+
+	def wholenumber(a,b):
+	answer = a//b
+	return answer
+
+	
